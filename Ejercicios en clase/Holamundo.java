4383ee4bd63f0d5867c26f4f88acecdabe3536a3
gla2sinsti <acargal2603@g.educaand.es>
2023-10-04 21:09:41 +0200

    primera subida

---
+++ Ejercicios en clase/Holamundo.java
@@ -0,0 +1,12 @@
+import javax.print.DocFlavor.STRING;
+
+/**
+ * Programa que imprime HolaMundo
+ * @author Adrian Carmona 
+ * @version 1.0
+ */
+public class Holamundo {
+  public static void main(String[] args) {
+      System.out.println("HOLA MUNDO");
+  }
+}
