e89b923d861bbd1af577b1b59122925d169a862e
gla2sinsti <acargal2603@g.educaand.es>
2023-11-22 21:12:54 +0100

    ejercicio_33_mejorado

--- Unidad_5/Ejercicios/Ejercicio_33/src/App.java
+++ Unidad_5/Ejercicios/Ejercicio_33/src/App.java
@@ -3,19 +3,18 @@ public class App {
         // titulo y preguntando la altura
         System.out.println("ejercicio_33");
         System.out.print("Introduzca la altura de la U: ");
-        int num = Integer.parseInt(System.console().readLine());
-        int altura = num; // metemos el dato ingresado en altura
+        int altura = Integer.parseInt(System.console().readLine());
         int anchura = altura; // metemos la altura e anchura ya que la U es 
 
         for (int i = 0; i < altura; i++) { // altura
             for (int j = 0; j < anchura; j++) {
-                if (i == altura - 1) {
+                if (i == altura - 1) { // si es el final de la U
                     if (j == 0 || j == anchura - 1) {
                         System.out.print("-"); // imprimimos espacios
                     } else {
                         System.out.print("*"); // imprimimos asteriscos
                     }
-                } else {
+                } else { // para hacer el resto de la U
                     if (j == 0 || j == anchura - 1) {
                         System.out.print("*"); // imprimimos asteriscos
                     } else {
