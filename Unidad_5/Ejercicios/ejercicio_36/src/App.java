c355e1b12f0424e4da75afaa66ea12719d55c454
gla2sinsti <acargal2603@g.educaand.es>
2023-11-24 19:00:47 +0100

    ejercicio_36

--- Unidad_5/Ejercicios/ejercicio_36/src/App.java
+++ Unidad_5/Ejercicios/ejercicio_36/src/App.java
@@ -6,8 +6,27 @@ public class App {
             System.out.print("Por favor, introduzca un número entero positivo: ");
             num = Integer.parseInt(System.console().readLine()); // pedimos un long 
         } while (num < 0);
+        boolean capicua = true; // El buleano que comprueba si es capicua al menos que cambie es verdadero
+        long alreves = vuelta(num); // para hacer una variable con el numero alreves
 
-        int logitud_num = 0;
-        for (int i = 0; i < num ; i++) {logitud_num++;}//longitud total numero
+
+        for (int i = 10; num/i > 0; i*=10) {
+            if (num%i != alreves%i) {capicua = false;}// si no es capicua
+        }
+
+        if (capicua == true) { // escribimos si es capicua o no
+            System.out.println("Es "+num+" es capicua.");
+        } else {
+            System.out.println("No es "+num+" es capicua.");
+        }
+    }
+
+    public static long vuelta(long num) {
+        long tmp = 0; // una variable temporal para darle la vuelta al numero
+        for (long i = num; i > 0; i/=10) {
+            tmp *= 10; // adelantamos el numero a la izquierda (si es 0 no hace nada)
+            tmp = tmp + (i % 10); // metemos el dato qe queremos en la casilla
+        }
+        return tmp; // devolvemos el numero hecho
     }
 }
