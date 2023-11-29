1d5748c2c2a1e3d78e5144a22244df22499dabd8
Adricarmona <acargal2603@g.educaand.es>
2023-11-29 02:21:46 +0100

    ejercicio 50
    si miras el del maestro es hasta facil cago en die

--- Unidad_5/Ejercicios/ejercicio_50/src/App.java
+++ Unidad_5/Ejercicios/ejercicio_50/src/App.java
@@ -8,7 +8,76 @@ public class App {
         if (altura < 5 || espacios < 1) { // comprovamos que los numeros ingresados son los correctos
             System.out.println("altura o espacios no validos.");
         } else {
-            
+            // parte de arriba
+            // primera linea
+            System.out.print("*");
+            for (int i = 0; i < espacios; i++) {
+                System.out.print(" ");
+            }
+            System.out.print("****");
+            for (int i = 0; i < espacios; i++) {
+                System.out.print(" ");
+            }
+            System.out.println("****");
+
+            // segunda linea y tercera linea
+            for (int i = 0; i < 2; i++) {
+                System.out.print("*");
+                for (int j = 0; j < espacios; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+                for (int j = 0; j < espacios + 3; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println("*");
+            }
+
+            // cuarta linea
+            System.out.print("*");
+            for (int i = 0; i < espacios; i++) {
+                System.out.print(" ");
+            }
+            System.out.print("****");
+            for (int i = 0; i < espacios; i++) {
+                System.out.print(" ");
+            }
+            System.out.println("****");
+
+            // empieza la 2 mitad
+            for (int i = 0; i < altura - 3; i++) {
+                System.out.print("*");
+                for (int j = 0; j < espacios + 3; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+                for (int j = 0; j < espacios + 3; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println("*");
+            }
+
+            // ultima linea
+            System.out.print("*");
+            for (int i = 0; i < espacios; i++) {
+                System.out.print(" ");
+            }
+            System.out.print("****");
+            for (int i = 0; i < espacios; i++) {
+                System.out.print(" ");
+            }
+            System.out.println("****");
         }
     }
 }
+
+
+/*
+
+* **** ****
+* *    *
+* **** ****
+*    *    *
+* **** ****
+
+*/
