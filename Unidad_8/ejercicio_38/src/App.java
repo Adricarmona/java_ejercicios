08b10784c7f10615b9e91d419ed30702b6dafe91
Adricarmona <adricarmonagalvez@gmail.com>
2024-01-26 13:51:02 +0100

    un ejercicio asi guapeton

--- Unidad_8/ejercicio_38/src/App.java
+++ Unidad_8/ejercicio_38/src/App.java
@@ -1,11 +1,29 @@
+import funciones.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] test = {1321,442244,102021};
+        test = filtraCapicuas(test);
+        for (int i = 0; i < test.length; i++) {
+            System.out.println(test[i]);
+        }
     }
 
     public static int[] filtraCapicuas(int[] x) {
+        int[] tmp = new int[x.length];
+        int logtmp = 0;
+        for (int i = 0; i < x.length; i++) {
+            if (generales.esCapicua(x[i])) {
+                tmp[logtmp] = x[i];
+                logtmp++;
+            }
+        }
         
+        // el resto en -1
+        for (int i = logtmp; i < tmp.length; i++) {
+            tmp[i] = -1;
+        }
 
-        return x;
+        return tmp;
     }
 }
