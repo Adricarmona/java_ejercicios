3e2ceb83d6853ddaf1898c15596ac8d340a8403d
Adricarmona <adricarmonagalvez@gmail.com>
2024-02-02 00:43:27 +0100

    Unidad 14

---
+++ Unidad_14/ejercicio_2/src/ExcepcionApareamientoImposible.java
@@ -0,0 +1,6 @@
+public class ExcepcionApareamientoImposible extends Exception { 
+    public ExcepcionApareamientoImposible() { 
+        System.out.println("ExcepcionApareamientoImposible: No se pueden aparear dos gatos del mismo genero"); 
+    } 
+}
+    
--- Unidad_14/ejercicio_2/src/TestGatos.java
+++ Unidad_14/ejercicio_2/src/TestGatos.java
@@ -1,3 +1,14 @@
 public class TestGatos {
-    
+    public static void main(String[] args) {
+        Gato mujerGato = new Gato(Sexo.HEMBRA);
+        Gato hombreGato = new Gato(Sexo.MACHO);
+        try {
+            hombreGato.apareaCon(mujerGato);
+            //hombreGato.apareaCon(hombreGato);
+            System.out.println("funciona");
+        } catch (ExcepcionApareamientoImposible EAI) {
+            System.out.println("EAI:"+EAI);
+        }
+    }
 }
+
