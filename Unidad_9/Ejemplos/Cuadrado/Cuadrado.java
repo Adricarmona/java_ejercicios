070d236767856dfdde7ae7effe1fe2f5a03bfae5
Adricarmona <adricarmonagalvez@gmail.com>
2024-02-01 22:41:13 +0100

    ejercicio ejemplo unidad 9

---
+++ Unidad_9/Ejemplos/Cuadrado/Cuadrado.java
@@ -0,0 +1,29 @@
+package Ejemplos.Cuadrado;
+
+public class Cuadrado {
+    int lado;
+    public Cuadrado(int l) {
+        this.lado = l;
+    }
+    public String toString() {
+        int i, espacios;
+        String resultado = "";
+        for (i = 0; i < this.lado; i++) {
+            resultado += "##";
+        }
+        resultado += "\n";
+
+        for (i = 1; i < this.lado - 1; i++) {
+            resultado += "##";
+            for (espacios = 1; espacios < this.lado - 1; espacios++) {
+                resultado += "  ";
+            }
+            resultado += "##\n";
+        }
+        for (i = 0; i < this.lado; i++) {
+            resultado += "##";
+        }
+        resultado += "\n";
+        return resultado;
+    }
+}
---
+++ Unidad_9/Ejemplos/Cuadrado/PruebaCuadrado.java
@@ -0,0 +1,8 @@
+package Ejemplos.Cuadrado;
+public class PruebaCuadrado {
+    public static void main(String[] args) {
+        Cuadrado miCuadradito = new Cuadrado(5);
+        System.out.println(miCuadradito);
+    }
+}
+    
