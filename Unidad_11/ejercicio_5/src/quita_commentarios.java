f8ffb80ad709e5d136b46377c6dae55f3d8377e8
Adricarmona <adricarmonagalvez@gmail.com>
2024-04-24 18:18:46 +0200

    por que esto no esta subido ya

--- Unidad_11/ejercicio_5/src/quita_commentarios.java
+++ Unidad_11/ejercicio_5/src/quita_commentarios.java
@@ -1,69 +1,89 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class quita_commentarios {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ejercico 5");
+        System.out.println("Ejercico 5\n");
 
         try {
-            comrpobadorExistencia(args[0]);
+            comprobadorExistencia(args[0], args[1]);
         } catch (Exception e) {
             System.out.println("Error: Se esperaban dos parametros");
         }
+
     }
 
 
     // funcin que comprueba la existencia del archivo
-    public static void comrpobadorExistencia(String primerParametro){
+    public static void comprobadorExistencia(String primerParametro, String segundoParametro){
         try {
             BufferedReader archivoLectura = new BufferedReader(new FileReader(primerParametro));
-            eliminadorComentarios(sacaComentarios(archivoLectura)); 
+            escibidor(eliminadorComentarios(sacaComentarios(archivoLectura)), segundoParametro);
             archivoLectura.close();
         } catch (FileNotFoundException e) {
             System.out.println("Error: El archivo no existe");
         } catch (Exception e) {
-            System.out.println("Error: Error no barajado");
+            System.out.println("Error: Error no barajado\n\n"+e);
         }
     }
 
     // saca los comentarios de los archivos
-    public static ArrayList<String> sacaComentarios(BufferedReader archivo) throws IOException {
-        ArrayList<String> texto = new ArrayList<String>();
+    public static String sacaComentarios(BufferedReader archivo) throws IOException {
+        String texto = "";
         String linea = "";
+
         do {
             linea = archivo.readLine();
-            if (linea != null) {
-                texto.add(linea+"\n");                
-            }
+            if (linea != null) {texto += linea+"\n";}
         } while (linea != null);
 
         return texto;
     }
 
     // comrpueba donde tiene los comentarios
-    public static void eliminadorComentarios(ArrayList<String> lista) {
+    public static String eliminadorComentarios(String lista) {
         boolean escribe = true;
+        boolean escribeSinSalto = true;
+        boolean variasLineas = false;
         String archivo = "";
-        for (String frase : lista) {
-            for (int i = 0; i < frase.length(); i++) {
-                if (escribe) {
-                    if (frase.charAt(i) == '/' && frase.charAt(i+1) == '/') {
-                        escribe = false;
-                    } else {
-                        archivo += frase.charAt(i);
-                    }
+
+        int posicion = 0;
+        while (posicion < lista.length()) {
+            if (escribe && escribeSinSalto) {
+                if (lista.charAt(posicion) == '/' && lista.charAt(posicion + 1) == '/') {
+                    escribe = false;
+                } else if (lista.charAt(posicion) == '/' && lista.charAt(posicion + 1) == '*') {
+                    escribeSinSalto = false;
                 } else {
-                    System.out.println(frase.charAt(i)+frase.charAt(i+1));
-                    if (frase.charAt(i) == '\\' && frase.charAt(i+1) == 'n') {
-                        escribe = true;
-                        archivo += "\n";
+                    archivo += lista.charAt(posicion);
+                }
+            } else {
+                if (lista.charAt(posicion) == '\n') {
+                    escribe = true;
+                    if (!escribeSinSalto && !variasLineas) {
+                        archivo += '\n';
+                        variasLineas = true;
                     }
+                } else if (lista.charAt(posicion) == '*' && lista.charAt(posicion + 1) == '/') {
+                    escribeSinSalto = true;
+                    variasLineas = false;
+                    posicion++;
                 }
             }
+            posicion++;
         }
-        System.out.println(archivo);
+
+        return archivo;
+    }
+
+    // Escribe en el archivo indicado
+    public static void escibidor(String primerArchivo, String segundoArchivo) throws IOException {
+        BufferedWriter archivoAEscribir = new BufferedWriter(new FileWriter(segundoArchivo));
+        archivoAEscribir.write(primerArchivo);
+        archivoAEscribir.close();
     }
 }
---
+++ Unidad_11/ejercicio_6/README.md
@@ -0,0 +1,117 @@
+ejercicio 6
+
+# indice
+
+1. Encabezados
+2. Cambios tipográficos
+3. Lista sin orden y con orden
+4. Snippets
+5. Enlaces
+6. Imágenes
+7. Tablas
+8. Líneas horizontales
+9. Salto de línea
+10. Lista de tareas
+11. Emoji
+12. Referencias
+13. Fórmulas Matemáticas 
+---
+
+## Encabezados
+
+# titulo nivel 1
+
+## titulo nivel 2
+
+### titulo nivel 3
+
+#### titulo nivel 4
+
+---
+
+## Cambios tipográficos
+
+_negrita_
+cursiva
+**_negrita y cursiva_**
+~~Tachado~~
+<mark>Remarcado</mark>
+
+## Listas
+
+### Sin Orden
+
+- Elemento 1
+  - Subelemento 1.0
+- Elemento 2
+  -Subelemento 2.1
+- Elemento 3
+
+### Ordenadas
+
+1. Primer elemento
+   1. Subelemento A
+   2. Subelemento B
+2. Segundo elemento
+3. Tercer elemento
+
+### Snippets de código y citas
+
+```java
+System.out.println ("Esto es un snippets a lo casual");
+
+<div>
+    <h1>Página web</h1>
+    <p>Esto es otro tipo de snippets</p>
+</div>
+```
+---
+
+#### Enlaces
+[enlace externo](https://www.markdownguide.org/getting-started/)
+
+#### Imagenes
+![foto del logo de la web](https://th.bing.com/th/id/OIP.u_BVa-CKyRuG_R9OHlq_QwHaEo?rs=1&pid=ImgDetMain)
+
+##### Foto Local
+![foto de el equipo](./ana.jp)
+
+## Citas
+> Esto se supone que es una cita
+
+
+#### Tablas
+|Nombres      | Edad        | Calificaciones|
+|-------------|-------------|---------------|
+| José        |   23        |         7,8   |
+| Miguel      |   21        |          9,9  |
+
+
+#### Lineas horizontales
+-----------
+.......
+ -----------
+
+#### Salto de linea 
+me llamo  
+paula
+
+#### Lista de tareas 
+- [x] Activado
+- [ ] No es activado 
+- [ ] Tampoco es activado
+
+#### Emoji
+carita sonriente :joy:  
+estrella :star:  
+emoji con gafas :sunglasses:
+
+#### Referencias 
+Este es un texto con una referencia [^1].
+ [^1]: Esta es la referencia
+
+
+ ## Fórmulas Matemáticas:
+
+ La fórmula de 2 grado es:
+ $(-b ± √ (b^2 – 4ac)) / (2a)$
